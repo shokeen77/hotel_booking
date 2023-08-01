c6e3a4b459bd51edd164a447a994bb451d611342
Nishant <shokeennishant444@gmail.com>
2023-08-01 15:19:29 +0530

    changes

--- src/component/ImageGrid.jsx
+++ src/component/ImageGrid.jsx
@@ -13,19 +13,20 @@ export default function ImageGrid(props) {
  
 
   useEffect(() => {
-    async function imageCollection() {
-      try {
-        const result = await fetch(`https://hotels-api-4ltr.onrender.com/api/hotels/${props.slug}`);
-        let jsonData = await result.json();
-        setApiData(jsonData);
-        setLoadingApi(false);
-      }
-      catch (error) {
-        console.log(error);
-      }
-  
-    }imageCollection();
-  }, [])
+     async function imageCollection() {
+    try {
+      const result = await fetch(`https://hotels-api-4ltr.onrender.com/api/hotels/${props.slug}`);
+      let jsonData = await result.json();
+      setApiData(jsonData);
+      setLoadingApi(false);
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+  };
+    imageCollection();
+  }, []);
   return (
     <div className="main">
       {apiLoading ? (
