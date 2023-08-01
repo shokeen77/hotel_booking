d1035c9f4a2f4961af99934475e619beda97bf1f
Nishant <shokeennishant444@gmail.com>
2023-08-01 15:24:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import HotelCard from './component/HotelCard';
 import "./styles.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
+import ImageGrid from './component/ImageGrid';
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <HotelCard></HotelCard>
-     
+     <ImageGrid slug = "Zion Kolob Canyon-Farm Stay-cows, goats, alpaca"></ImageGrid>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Form />} />
